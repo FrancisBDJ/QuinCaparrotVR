2f682bd07b1bbe567aa52ccfc8d48b2561c596da
FrancisBDJ <francisbartholomewdaniele@paucasesnovescifp.cat>
2023-02-19 19:41:45 +0100

    ButtonCheckerInteractable

--- Assets/Scripts/PlaceHolderChecker.cs
+++ Assets/Scripts/PlaceHolderChecker.cs
@@ -8,32 +8,44 @@ public class PlaceHolderChecker : MonoBehaviour
 {
     public List<bool> objectPlaced;
     public List<bool> caparrotsInPedestals;
-
     public List<GameObject> placeHolders;
     private GameManager _gameManager;
     private BlinkCheck _blinkCheck;
 
-    private bool win = false;
-    
     [SerializeField] private float pickupRange = 5.0f;
+
+
+    public void CheckObjectsPlacedInPedestals()
+    {
+        for (int i = 0; i < objectPlaced.Count; i++)
+        {
+            if (objectPlaced[i])
+            {
+                placeHolders[i].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            else
+            {
+                placeHolders[i].GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
         _blinkCheck = FindObjectOfType<BlinkCheck>();
 
-        objectPlaced = Enumerable.Repeat(false,placeHolders.Count).ToList();
-        caparrotsInPedestals = Enumerable.Repeat(false,placeHolders.Count).ToList();
+        objectPlaced = Enumerable.Repeat(false, placeHolders.Count).ToList();
+        caparrotsInPedestals = Enumerable.Repeat(false, placeHolders.Count).ToList();
     }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,
-                    pickupRange))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
             {
                 if (hit.transform.CompareTag("Button") && _gameManager.win.Value != true)
                 {
@@ -58,15 +70,15 @@ public class PlaceHolderChecker : MonoBehaviour
                         if (objects == false)
                         {
                             win = false;
-                            _gameManager.points -= 200; 
-                            
-                            if (_gameManager.points <= 0) 
+                            _gameManager.points -= 200;
+
+                            if (_gameManager.points <= 0)
                             {
                                 _gameManager.points = 0;
                             }
                         }
                     }
-                    
+
                     if (win)
                     {
                         _gameManager.win.Value = true;
