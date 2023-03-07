eb75bc2bcf8042a4d04a15b68a0c701be59009a0
FrancisBDJ <francisbartholomewdaniele@paucasesnovescifp.cat>
2023-03-07 09:48:24 +0100

    MainMenu funcional

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,17 +9,40 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private Button _startGameBtn;
+    [SerializeField] private Button _optionsBtn;
+    [SerializeField] private Button _quitBtn;
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Level1");
-    }
+    
     
     
     // Start is called before the first frame update
     void Start()
     {
         _startGameBtn.onClick.AddListener(StartGame);
+        _optionsBtn.onClick.AddListener(Options);
+        _quitBtn.onClick.AddListener(Quit);
         Debug.Log("Main menu manager initialized");
+    }
+    
+    private void StartGame()
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        if(EditorApplication.isPlaying) 
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+#else
+            Application.Quit();
+#endif
+    }
+
+    private void Options()
+    {
+        SceneManager.LoadScene("Options");
     }
 }
