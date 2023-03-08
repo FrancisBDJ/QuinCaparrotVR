3da52d0ebb47a25c5835cefded9df4d079d94a4a
FrancisBDJ <francisbartholomewdaniele@paucasesnovescifp.cat>
2023-03-08 12:33:59 +0100

    Attach control disabled, creado prefab XR Origin Level1

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class GameOverManager : MonoBehaviour
 {
     [SerializeField] private Button btnRestart;
     [SerializeField] private Button btnMenu;
     [SerializeField] private Button btnQuit;
+    [SerializeField] private GameObject losePanel;
+    [SerializeField] private GameObject winPanel;
     // Start is called before the first frame update
     void Start()
     {
         btnRestart.onClick.AddListener(Restart);
         btnMenu.onClick.AddListener(Menu);
         btnQuit.onClick.AddListener(Quit);
+        
     }
 
     // Update is called once per frame
